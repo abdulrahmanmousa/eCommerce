5d297ea9d39ec381ffd3638b6f8fa78e179c8eef
abdulrahman mousa <abdomousa20102011@gmail.com>
2023-07-10 02:08:21 +0300

    last

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,12 @@
 import Slider from "../components/Slider";
 import "./Home.scss";
 import FeaturedProducts from "../components/FeaturedProducts";
-import Categories from "../components/Categories";
 
 export default function Home() {
   return (
     <>
       <Slider />
       <FeaturedProducts />
-      <Categories />
     </>
   );
 }
